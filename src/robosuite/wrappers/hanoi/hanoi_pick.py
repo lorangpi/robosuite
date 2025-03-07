5c1cfa24608187e2baa0f5a2ab93c3fccf8158c3
lorangpi <pierrick.lorang@gmail.com>
2025-03-07 17:04:49 -0500

    improved reward function

--- src/robosuite/wrappers/hanoi/hanoi_pick.py
+++ src/robosuite/wrappers/hanoi/hanoi_pick.py
@@ -260,6 +260,7 @@ class HanoiPickWrapper(gym.Wrapper):
             obs, reward, terminated, info = self.env.step(action)
         self.env.render() if self.render_init else None
         state = self.detector.get_groundings(as_dict=True, binary_to_float=False, return_distance=False)
+        distances = self.detector.get_groundings(as_dict=True, binary_to_float=False, return_distance=True)
         success = state[f"picked_up({self.obj_to_pick})"]
         info['is_sucess'] = success
         if success:
@@ -271,25 +272,28 @@ class HanoiPickWrapper(gym.Wrapper):
             obs = self.filter_obs(obs)
         if success:
             reward = 10
-        elif state[f"over(gripper,{self.obj_to_pick})"]:
-            pick_pos = self.env.sim.data.body_xpos[self.obj_mapping[self.obj_to_pick]][2]
-            gripper_pos = self.env.sim.data.body_xpos[self.env.gripper_body][2]
-            dist = np.abs(gripper_pos - pick_pos)   
-            reward = -4 - (np.tanh(50.0 * dist))
-        elif state[f"over(gripper,{self.obj_to_pick})"] and state[f"open(gripper)"]:
-            reward = -3.5
-        elif state[f"over(gripper,{self.obj_to_pick})"] and state[f"at_grab_level(gripper,{self.obj_to_pick})"]:
-            reward = -3
         elif state[f"grasped({self.obj_to_pick})"]:
             z_target = self.env.table_offset[2] + 0.45
             object_z_loc = self.env.sim.data.body_xpos[self.obj_mapping[self.obj_to_pick]][2]
             z_dist = z_target - object_z_loc
-            reward = -1 - (np.tanh(50.0 * z_dist))
+            reward = -1 - (np.tanh(10 * z_dist))
+        elif state[f"over(gripper,{self.obj_to_pick})"] and state[f"at_grab_level(gripper,{self.obj_to_pick})"] and state[f"open(gripper)"]:
+            reward = -2
+        elif state[f"over(gripper,{self.obj_to_pick})"] and state[f"at_grab_level(gripper,{self.obj_to_pick})"]:
+            reward = -3.2
+        elif state[f"over(gripper,{self.obj_to_pick})"] and state[f"open(gripper)"]:
+            pick_pos = self.env.sim.data.body_xpos[self.obj_mapping[self.obj_to_pick]][2]
+            gripper_pos = self.env.sim.data.body_xpos[self.env.gripper_body][2]
+            dist = np.abs(gripper_pos - pick_pos)   
+            reward = -3 - (np.tanh(10 * dist))
+        elif state[f"over(gripper,{self.obj_to_pick})"]:
+            aperture = distances[f"open(gripper)"]
+            reward = -5 + aperture
         else:
             pick_pos = self.env.sim.data.body_xpos[self.obj_mapping[self.obj_to_pick]][:2]
             gripper_pos = self.env.sim.data.body_xpos[self.env.gripper_body][:2]
             dist = np.linalg.norm(gripper_pos - pick_pos)
-            reward = -6 - (np.tanh(50.0 * dist))
+            reward = -6 - (np.tanh(5 * dist))
         self.step_count += 1
         if self.step_count > self.horizon:
             terminated = True
